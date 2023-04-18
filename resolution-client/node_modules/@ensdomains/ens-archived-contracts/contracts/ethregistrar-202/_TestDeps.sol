07e1b0f9106e656b56cf1cc118b962d8fc1c48d3
boray <bsaygilier33@gmail.com>
2023-04-19 01:37:10 +0300

    ❌ remove node_modules

--- resolution-client/node_modules/@ensdomains/ens-archived-contracts/contracts/ethregistrar-202/_TestDeps.sol
+++
@@ -1,3 +0,0 @@
-// This file  exists to persuade Truffle to import and build contracts we depend
-// on in our tests.
-import "@ensdomains/ens/contracts/ENSRegistry.sol";
--- resolution-client/node_modules/@fuel-ts/versions/dist/cli.d.ts
+++
@@ -1,3 +0,0 @@
-declare function run(): void;
-
-export { run };
